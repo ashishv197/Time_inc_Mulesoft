8bd30178521f37020dfa763da0f0e262b15432d6
Ashish Verma <vermaa@timeinc.com>
2017-11-26 23:55:26 +0530

    removed unwanted projects

--- hellotime_inc_ver1/deploy.bat
+++
@@ -1,2 +0,0 @@
-mvn -Danypoint.username=<username>  -Danypoint.password=<password>  -Danypoint.environment=<EnvironmentName>  -Danypoint.applicationName=<ApplicationName>    -Danypoint.platform.client_id=<Client_ID>  -Danypoint.platform.client_secret=<Client_Secret> -s settings.xml clean -DskipTests package mule:deploy -P cloudhub
-
